729886fca975f0911d533cd4429a4bf97c75302b
Royer <rvz2@njit.edu>
2018-10-03 21:19:25 -0400

    New folder with html and php files

---
+++ login/login.php
@@ -0,0 +1,20 @@
+<?php
+$email = $_POST["email"];
+$password = $_POST["password"];
+printValue($email.$password);
+
+function checkEmpty() {
+
+}
+
+function checkAt() {
+
+}
+
+function check8Chars() {
+
+}
+
+function printValue($var) {
+    print($var);
+}
